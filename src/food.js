7c7061e88110d1d531efb204417fe9d88508dbe3
mukesh <mukeshvishwakarma251003@gmail.com>
2025-10-29 13:00:46 +0530

    first commit

---
+++ src/food.js
@@ -0,0 +1,47 @@
+import image1 from "./assets/del-img1.jpg"
+import image2 from "./assets/del-img2.jpg"
+import image3 from "./assets/del-img3.jpg"
+import image4 from "./assets/del-img4.jpg"
+import image5 from "./assets/del-img5.jpg"
+import image6 from "./assets/del-img6.jpg"
+import image7 from "./assets/del-img7.jpg"
+import image8 from "./assets/del-img8.jpg"
+import image9 from "./assets/del-img9.jpg"
+import image10 from "./assets/del-img10.jpg"
+import image11 from "./assets/del-img11.jpg"
+import image12 from "./assets/del-img12.jpg"
+import image13 from "./assets/del-img13.jpg"
+import image14 from "./assets/del-img14.jpg"
+import image15 from "./assets/del-img15.jpg"
+import image16 from "./assets/del-img16.jpg"
+import image17 from "./assets/del-img17.jpg"
+import image18 from "./assets/del-img18.jpg"
+import image19 from "./assets/del-img19.jpeg"
+import image20 from "./assets/del-img20.jpg"
+import image21 from "./assets/del-img21.jpg"
+import image22 from "./assets/del-img22.jpg"
+
+export const food_items = [
+    {"id":1, "food_name":"Pancakes", "food_category":"Breakfast", "food_type":"veg", "food_quantity":1, "food_image": image1, "price": 499},
+    {"id":2, "food_name":"Chicken Soup", "food_category":"Soups", "food_type":"non_veg", "food_quantity":1, "food_image": image2, "price": 399},
+    {"id":3, "food_name":"Minestrone Soup", "food_category":"Soups", "food_type":"veg", "food_quantity":1, "food_image": image3, "price": 349},
+    {"id":4, "food_name":"Spaghetti Carbonara", "food_category":"Pasta", "food_type":"non_veg", "food_quantity":1, "food_image": image4, "price": 999},
+    {"id":5, "food_name":"Veg Alfredo Pasta", "food_category":"Pasta", "food_type":"veg", "food_quantity":1, "food_image": image5, "price": 899},
+    {"id":6, "food_name":"Veggie Burger", "food_category":"Burger", "food_type":"veg", "food_quantity":1, "food_image": image6, "price": 499},
+    {"id":7, "food_name":"Chicken Burger", "food_category":"Burger", "food_type":"non_veg", "food_quantity":1, "food_image": image7, "price": 599},
+    {"id":8, "food_name":"Tomato Soup", "food_category":"Soups", "food_type":"veg", "food_quantity":1, "food_image": image8, "price": 299},
+    {"id":9, "food_name":"Mushroom Soup", "food_category":"Soups", "food_type":"non_veg", "food_quantity":1, "food_image": image9, "price": 349},
+    {"id":10, "food_name":"Egg Sandwich", "food_category":"Breakfast", "food_type":"non_veg", "food_quantity":1, "food_image": image10, "price": 349},
+    {"id":11, "food_name":"Cheese Omelette", "food_category":"Breakfast", "food_type":"non_veg", "food_quantity":1, "food_image": image11, "price": 399},
+    {"id":12, "food_name":"Hash Browns", "food_category":"Breakfast", "food_type":"veg", "food_quantity":1, "food_image": image12, "price": 249},
+    {"id":13, "food_name":"Chicken Alfredo Pasta", "food_category":"Pasta", "food_type":"non_veg", "food_quantity":1, "food_image": image13, "price": 1099},
+    {"id":14, "food_name":"Hash Browns", "food_category":"Pasta", "food_type":"non_veg", "food_quantity":1, "food_image": image14, "price": 949},
+    {"id":15, "food_name":"Paneer Butter Masala", "food_category":"Main_course", "food_type":"veg", "food_quantity":1, "food_image": image15, "price": 749},
+    {"id":16, "food_name":"Chicken Biryani", "food_category":"Main_course", "food_type":"non_veg", "food_quantity":1, "food_image": image16, "price": 949},
+    {"id":17, "food_name":"Chicken Tikka Masala", "food_category":"Main_course", "food_type":"non_veg", "food_quantity":1, "food_image": image17, "price": 1199},
+    {"id":18, "food_name":"Fish and Chips", "food_category":"Main_course", "food_type":"non_veg", "food_quantity":1, "food_image": image18, "price": 1099},
+    {"id":19, "food_name":"Margherita Pizza", "food_category":"Pizza", "food_type":"veg", "food_quantity":1, "food_image": image19, "price": 649},
+    {"id":20, "food_name":"Pepperoni Pizza", "food_category":"Pizza", "food_type":"non_veg", "food_quantity":1, "food_image": image20, "price": 799},
+    {"id":21, "food_name":"Garlic Bread", "food_category":"Pizza", "food_type":"veg", "food_quantity":1, "food_image": image21, "price": 299},
+    {"id":22, "food_name":"Hawaiian Pizza", "food_category":"Pizza", "food_type":"non_veg", "food_quantity":1, "food_image": image22, "price": 799},
+]
